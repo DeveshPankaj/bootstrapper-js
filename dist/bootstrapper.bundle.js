cf3b501b82f257d0f30df7e3bb9bd724923f0835
Pankaj Devesh <deveshpankaj@github.com>
2023-09-29 00:21:48 +0530

    added root file path

--- dist/bootstrapper.bundle.js
+++ dist/bootstrapper.bundle.js
@@ -7,7 +7,7 @@ var __webpack_exports__ = {};
 
 const __BOOTSTRAP_SCRIPT_PATH_KEY__ = '__BOOTSTRAP_SCRIPT_PATH__';
 const loadBootstrapScript = (storage) => {
-    const bootstrap_script_path = storage.getItem(__BOOTSTRAP_SCRIPT_PATH_KEY__);
+    const bootstrap_script_path = storage.getItem(__BOOTSTRAP_SCRIPT_PATH_KEY__) || "dist/remote.bundle.js";
     if (!bootstrap_script_path)
         return;
     const script = window.document.createElement('script');
