fb054280c7543e37c4d01f6ff240d0a3f2929896
Pankaj Devesh <deveshpankaj@github.com>
2024-08-18 01:11:06 +0530

    Sync

---
+++ docs/public/mount/usr/desktop/temp.js
@@ -0,0 +1,19 @@
+const platform = this
+const React = window.platform.getService('React')
+const ReactDOM = window.platform.getService('ReactDOM')
+console.log(platform)
+
+
+exports.React = React
+exports.ReactDOM = ReactDOM
+
+
+
+const el = document.createElement('div')
+const root = ReactDOM.createRoot(el)
+platform.window.document.body.appendChild(el)
+
+const App = () => {
+    return React.createElement('div', {}, ["Hello"])
+}
+root.render(App())
