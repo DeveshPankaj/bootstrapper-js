1ac0544a8d1c2eab3f98d07957d7edcc823deb58
Pankaj Devesh <deveshpankaj@github.com>
2024-08-18 10:52:10 +0530

    Sync

--- src/apps/file-explorer/desktop.tsx
+++ src/apps/file-explorer/desktop.tsx
@@ -12,14 +12,14 @@ export const ListDirComponent = ({ dir, openFile, showFileActions }: { dir?: str
     dir ??= DESKTOP_PATH;
 
     const extIconMap: Record<string, string> = {
-        '.js': '/js-icon.png',
-        '.ts': '/ts-icon.png',
-        '.proj': '/game-icon.png',
-        '.html': '/html-icon.png',
-        '.png': '/png-icon.png',
-        '.run': '/bash.png',
-        '.': '/folder-icon.png',
-        '': '/invalid-file-icon.png'
+        '.js': '/public/js-icon.png',
+        '.ts': '/public/ts-icon.png',
+        '.proj': '/public/game-icon.png',
+        '.html': '/public/html-icon.png',
+        '.png': '/public/png-icon.png',
+        '.run': '/public/bash.png',
+        '.': '/public/folder-icon.png',
+        '': '/public/invalid-file-icon.png'
     }
 
     const fs = platform.host.getFS()
