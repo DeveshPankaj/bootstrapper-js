b21e307bbf5881404d39dd9ab416759a7a2025e0
Pankaj Devesh <deveshpankaj@github.com>
2024-09-30 21:01:44 +0530

    updated react/babel demo

--- docs/public/mount/home/user1/temp.js
+++ docs/public/mount/home/user1/temp.js
@@ -4,14 +4,28 @@ const ReactDOM = window.platform.getService('ReactDOM')
 exports.React = React
 exports.ReactDOM = ReactDOM
 
-
-
 // const el = document.createElement('div')
 // const root = ReactDOM.createRoot(el)
 // platform.window.document.body.appendChild(el)
 
+const Counter = () => {
+   const [counter, setCounter] = React.useState(0)
+   return <button onClick={_ => setCounter(state => state+1)}>Clicked {counter} times</button>
+}
+
 const App = () => {
-    return React.createElement('div', {}, ["Hello World"])
+  const openThisFile = () => {
+    const cmd = `service('001-core.layout', 'open-window') (command('ui.notepad'), '/home/user1/temp.js')`
+    window.platform.host.execCommand(cmd)
+  }
+  return (
+    <>
+        <h3>This Is Babel and React's magic!</h3>
+        <Counter />
+        <p>Edit <span style={{color: 'blue', cursor: 'pointer'}} onClick={openThisFile}>'/home/user1/temp1.js'</span> and save to see the changes</p>
+    </>
+  )
+    // return React.createElement('div', {}, ["Hello World"])
 }
 exports.App = App
 // root.render(App())
