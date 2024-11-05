91350eace477ebe19501e2e599c0b80c19161818
Pankaj Devesh <deveshpankaj@github.com>
2024-11-06 02:10:13 +0530

    fixed typo

--- docs/public/mount/home/user1/apps/markdown.js
+++ docs/public/mount/home/user1/apps/markdown.js
@@ -3,7 +3,7 @@ const platform = window.platform;
 const run = (...args) => {
   console.log('Opening...')
   console.log(args)
-  platform.host.exec(platform, '/home/user1/apps/md-rander.html', args.slice(2))
+  platform.host.exec(platform, '/home/user1/apps/md-render.html', args.slice(2))
 }
 
 const { remove } = platform.host.registerCommand("ui.markdown", run, {
