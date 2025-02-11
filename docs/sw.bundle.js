8891d31d58ff25130be722e52206db4dae2fe016
Pankaj Devesh <deveshpankaj@github.com>
2025-02-11 20:26:29 +0530

    Sync

--- docs/sw.bundle.js
+++ docs/sw.bundle.js
@@ -68,7 +68,10 @@ self.addEventListener('fetch', function (event) {
     //     )
     // }
     const _url = new URL(url);
-    // console.log(_url.pathname)
+    if (_url.pathname.startsWith('/(sw)/')) {
+        console.log(_url.pathname);
+        event.respondWith(new Response("<h1>we are working on interprocess message passing</h1>", { headers: { 'Content-Type': "text/html" } }));
+    }
     if (_url.pathname.startsWith('/cache')) {
         event.respondWith(caches.open(cacheName).then((cache) => {
             // Respond with the image from the cache or from the network
