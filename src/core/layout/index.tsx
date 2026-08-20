d9aa6d0d4ccbb6abdc212405ec183df32f5775fc
Pankaj Devesh <deveshpankaj@github.com>
2026-08-20 23:16:53 +0530

    Remove Dashboard, Music Player, SQLite Browser from desktop context menu

--- src/core/layout/index.tsx
+++ src/core/layout/index.tsx
@@ -988,24 +988,6 @@ export const render = (container: HTMLElement) => {
             },
             {
                 type: 'action',
-                id: '7',
-                title: 'Dashboard',
-                cmd: `service('001-core.layout', 'open-window') (command('ui.dashboard'))`
-            },
-            {
-                type: 'action',
-                id: '9',
-                title: 'Music Player',
-                cmd: `service('001-core.layout', 'open-window') (command('ui.audioplayer'))`
-            },
-            {
-                type: 'action',
-                id: '10',
-                title: 'SQLite Browser',
-                cmd: `service('001-core.layout', 'open-window') (command('ui.sqlite'))`
-            },
-            {
-                type: 'action',
                 id: '11',
                 title: 'App Manager',
                 cmd: `service('001-core.layout', 'open-window') (command('ui.pkg-manager'))`
